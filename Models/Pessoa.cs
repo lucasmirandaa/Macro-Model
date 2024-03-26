14a9fb3f747f4fe1933fe22f4f45df01f7fcf818
Lucas Miranda <lucasmirandaa458@gmail.com>
2024-03-26 19:51:27 -0300

    Classe Pessoa

---
+++ Models/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Macro_Model.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        [Display(Name = "CPF")]
+        [Required(ErrorMessage = "Obrigatório informar CPF.")]
+        [MaxLength(11, ErrorMessage = "Somente números.")]
+        public int Cpf {  get; set; }
+        [Display(Name ="Nome")]
+        [Required(ErrorMessage = "Obrigatório")]
+        [MaxLength(50, ErrorMessage = "Máximo de 50 caracteres.")]
+        public int Nome { get; set; }
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "Obrigatório informar e-mail.")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Informe um e-mail válido.")]
+        public string Email { get;
+        [Display(Name = "Senha")]
+        [Required(ErrorMessage = "Obrigatório informar uma senha.")]
+        public string Senha { get; set; }
+    }
+}
